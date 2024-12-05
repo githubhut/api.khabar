db62a86c83b69390e7cfe9bba3b4ea113d1a1e54
Bhupender Chauhan <122470706+githubhut@users.noreply.github.com>
2024-12-05 19:04:26 +0530

    api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000; // You can use any port you prefer
+const port = 3001; // You can use any port you prefer
 
 // Middleware to handle JSON request bodies
 app.use(express.json());
@@ -17,13 +17,13 @@ app.get('/api/data', async (req, res) => {
     const p = req.query.page;
     const pageSize=req.query.pageSize;
     // Replace with the actual API URL you want to query
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=3705a1cedf15419caaa898fb5f01a905`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=3705a1cedf15419caaa898fb5f01a905`;
 
     // Make a GET request to the external API
     const response = await axios.get(apiUrl);
 
     // Send the JSON response from the external API back to the client
-    res.json(response.data);
+    res.send(response.data);
   } catch (error) {
     // Handle any errors that occur during the request
     console.error('Error querying external API:', error);
